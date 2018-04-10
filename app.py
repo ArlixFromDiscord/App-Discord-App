255045926664fae9b5c67c4c63d8762d7042444c
Arlix <35354893+ArlixFromDiscord@users.noreply.github.com>
2018-04-10 17:50:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-Anything not working? Report it to Issues and I will help you
+# Anything not working? Report it to Issues and I will help you
 import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
-import asyncio 
+import asyncio
 
-app = discord.App()
-bot_prefix= "!"
-App = commands.Bot(command_prefix=bot_prefix)
+app = commands.Bot(command_prefix='#')
 
 @app.event
 async def on_ready():
     print ("GitHub - https://www.github.com/ArlixFromDiscord/App-Discord-Bot")
+    print ("This is a test for the app")
 
-@app.command()
+@app.command(pass_context=True)
 async def credits(ctx):
     await app.say("This is a bot that was written in the App version of discord.py and someone downloaded it so this is the only credit I bet. - If you want this bot, go to https://www.github.com/ArlixFromDiscord/App-Discord-Bot")
 
-app.run("Icantbelivethatudownloadedthisinsteadoflookingatitandrewritingitokno")
+app.run("NDI0MjYzMjc3OTU1MjUyMjQ0.DacQcQ.DFBbTYD3MjI3uVmGdoedqyMmobg")
